56676a8a6eee13e22a36e2541768591f891183bd
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-10-16 16:04:39 -0300

    chore: estrutura inicial client/server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (_, res) => res.json({ status: 'ok' }));
+
+app.listen(4000, () => console.log('Backend running on port 4000'));
